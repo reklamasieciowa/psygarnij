491c0cb374f7fb25bf85e4ce9408b9e3b070ec50
reklamasieciowa <biuro@reklamasieciowa.pl>
2018-08-10 21:40:21 +0200

    models: animal, animaltype, apge, role and migrations made

---
+++ database/migrations/2018_08_10_191915_create_animals_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('animaltype_id');
+            $table->string('sex');
+            $table->integer('age');
+            $table->string('location');
+            $table->boolean('homeless')->default('1');
+            $table->string('avatar');
+            $table->text('description');
+            $table->dateTime('added');
+            $table->boolean('verified')->default('1');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
